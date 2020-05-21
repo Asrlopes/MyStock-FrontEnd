fcdae6e085782ee27cde998ffb060f42e397cb87
Amoresio Souza <amoresio.souza@unemat.br>
2020-05-21 03:46:43 -0400

    cria paginas

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: linear-gradient(180deg, #3892FF 0%, rgba(255, 255, 255, 0) 100%), #52A0FF;
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', serif;
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
